642dfff61942bfe0687026edc441473be1f807fb
BlairLAdams <BlairLAdams@sonic.net>
2018-12-16 16:13:46 -0800

    learnin

---
+++ Galvanize/sum_whole_numbers.py
@@ -0,0 +1,13 @@
+#!/Users/BlairLAdams/anaconda3/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed Dec 16 9:09:00 2018
+@author: BlairLAdams
+"""
+
+num = int(input('Please enter a number: '))
+dash, arrow = '-', '>' 
+
+arrow = dash*num + arrow
+print(arrow)
